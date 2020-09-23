c195635924db6beee26c370c6301d0e296554908
DiegoSperandioAR <dego.sperandio.rodriguez@gmail.com>
2020-09-22 20:16:05 -0400

    primer commit

---
+++ Tarea 1.py
@@ -0,0 +1,28 @@
+#1.- Completar y probar:
+def devolver_numero_5():
+  numero = 5
+  return numero
+
+#2.-Completar y probar:
+def devolver_una_lista():
+  lista = [5, 6, 7, 8, 9, 10]
+  return lista
+
+#3.- Completar y probar:
+def devolver_misma_lista(una_lista):
+  return una_lista
+
+#4.- Completar y probar:
+def devolver_nombre(nombre):
+  return nombre
+
+#5.- Completar y probar:
+def devolver_la_suma(numero1, numero2):
+  suma = numero1 + numero2
+  return suma
+
+#6.- Completar y probar:
+#< Definir una funcion que sea la multiplicacion de dos numeros >
+def multiplicacion_a_b(numero1, numero2):
+  resultado = numero1 * numero2
+  return resultado
