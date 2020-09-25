bf9befacaa66393fd9aa13807a76bd62acb903fb
DiegoSperandioAR <dego.sperandio.rodriguez@gmail.com>
2020-09-25 17:10:54 -0400

    Ésta es la segunda tarea, sobre las listas. Aún faltan algunos ejercicios

---
+++ tarea_listas.py
@@ -0,0 +1,37 @@
+# 1. Completar y probar:
+def devolver_longitud_de_un_nombre(unNombre):
+  return len(unNombre)
+
+# 2. Completar y probar:
+def devoler_primer_elemento_de_una_lista(unaLista):
+  return unaLista[0]
+
+# 3. Completar y probar:
+def devolver_segundo_elemento_del_nombre(unNombre):
+  return unNombre[1]
+
+# 4. Completar y probar:
+def devolver_ultimo_elemento_del_nombre(unNombre):
+  return unNombre[-1]
+
+# 5. Completar y probar:
+def devolver_la_letra_en_posicion_del_nombre(unNombre, posicion):
+  return unNombre[posicion]
+
+# 6. Completar y probar:
+def reemplazar_ultimo_elemento_de_la_lista(unaLista, unElemento):
+  unaLista[-1] = unElemento
+  return unaLista
+
+# 7. Completar y probar:
+def agregar_25_al_final_de_la_lista():
+  lista = ["casa", 5, "A"]
+  elemento = 25
+  lista.append(elemento)
+  return lista
+
+# 8. Completar y probar:
+def agregar_nombre_al_final_de_la_lista(unNombre):
+  lista = ["casa", 5, "A"]
+  lista.append(unNombre)
+  return lista
