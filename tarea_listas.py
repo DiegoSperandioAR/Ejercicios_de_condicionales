d60501e0387e17dd5837a48ccbb3240cf42e317a
DiegoSperandioAR <dego.sperandio.rodriguez@gmail.com>
2020-09-28 15:38:27 -0400

    Tarea resubida con todas las actividades completadas

--- tarea_listas.py
+++ tarea_listas.py
@@ -35,3 +35,29 @@ def agregar_nombre_al_final_de_la_lista(unNombre):
   lista = ["casa", 5, "A"]
   lista.append(unNombre)
   return lista
+
+# 9. Definir un función que recibe una lista de números y devuelve esa lista pero con el primer elemento
+# multiplicado por 2
+def multiplica_por_2_primer_numero_de_la_lista(lista_numeros):
+  lista_numeros[0] = lista_numeros[0] * 2
+  return lista_numeros
+
+# 10. Definir una función que recibe una lista de números y devuelve esa lista pero con el último elemento
+# multiplicado por 3
+def multiplica_por_3_ultimo_numero_de_la_lista(lista_numeros):
+  lista_numeros[-1] = lista_numeros[-1] * 3
+  return lista_numeros
+
+# 11. Definir un función que recibe una lista de números y devuelve esa lista pero con el primero y el
+# último elemento multiplicados por 2 y 3 respectivamente (todo en una única función pero usando
+# las funciones definidas en los dos puntos anteriores)
+def multiplica_por_2_y_3_los_numeros_de_la_lista(lista_numeros):
+  lista_numeros[0] = lista_numeros[0] * 2
+  lista_numeros[-1] = lista_numeros[-1] * 3
+  return lista_numeros
+
+# 12. Definir un función que recibe una lista y un elemento, y devuelve la lista con ese elemento agregado
+# al final
+def agregar_elemento_a_la_lista(lista, elemento):
+  lista.append(elemento)
+  return lista
